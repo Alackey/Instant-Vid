1b4034b3398200a6f1be4fdef6b42a669fead22f
Anthony Lackey <alackey96@gmail.com>
2016-10-27 19:53:18 -0700

    Instant vid init

---
+++ next.js
@@ -0,0 +1,30 @@
+// Wait and click the next button
+function waitForNext() {
+    setTimeout(function(){
+        var nextUpCard = document.getElementsByClassName("nextUpCard");
+        if (nextUpCard.length == 1) {
+            nextUpCard[0].click();
+        }
+
+        waitForNext();
+    }, 1000);
+}
+
+
+// Check if we are on an Amazon Instant Video page
+var count = 0;
+function onInstantVideo(){
+    setTimeout(function(){
+        count++;
+        var pause_buttons = document.getElementsByClassName("buttons");
+
+        if (pause_buttons.length === 1) {
+            waitForNext();
+        } else if (count != 10) {
+            onInstantVideo();
+        }
+    }, 1000);
+}
+
+
+onInstantVideo();
